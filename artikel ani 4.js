2fd04ab73bb62d034accb71d57ed393705819bc3
Aksal mek <56443790+axsalmek@users.noreply.github.com>
2020-03-08 23:36:53 +0700

    Update artikel ani 4.js

--- artikel ani 4.js
+++ artikel ani 4.js
@@ -1,27 +1,79 @@
-var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64._utf8_encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
-output=output+ this._keyStr.charAt(enc1)+ this._keyStr.charAt(enc2)+ this._keyStr.charAt(enc3)+ this._keyStr.charAt(enc4);}
-return output;},decode:function(input){var output="";var chr1,chr2,chr3;var enc1,enc2,enc3,enc4;var i=0;input=input.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(i<input.length){enc1=this._keyStr.indexOf(input.charAt(i++));enc2=this._keyStr.indexOf(input.charAt(i++));enc3=this._keyStr.indexOf(input.charAt(i++));enc4=this._keyStr.indexOf(input.charAt(i++));chr1=(enc1<<2)|(enc2>>4);chr2=((enc2&15)<<4)|(enc3>>2);chr3=((enc3&3)<<6)|enc4;output=output+ String.fromCharCode(chr1);if(enc3!=64){output=output+ String.fromCharCode(chr2);}
-if(enc4!=64){output=output+ String.fromCharCode(chr3);}}
-output=Base64._utf8_decode(output);return output;},_utf8_encode:function(string){string=string.replace(/\r\n/g,"\n");var utftext="";for(var n=0;n<string.length;n++){var c=string.charCodeAt(n);if(c<128){utftext+=String.fromCharCode(c);}
-else if((c>127)&&(c<2048)){utftext+=String.fromCharCode((c>>6)|192);utftext+=String.fromCharCode((c&63)|128);}
-else{utftext+=String.fromCharCode((c>>12)|224);utftext+=String.fromCharCode(((c>>6)&63)|128);utftext+=String.fromCharCode((c&63)|128);}}
-return utftext;},_utf8_decode:function(utftext){var string="";var i=0;var c=c1=c2=0;while(i<utftext.length){c=utftext.charCodeAt(i);if(c<128){string+=String.fromCharCode(c);i++;}
-else if((c>191)&&(c<224)){c2=utftext.charCodeAt(i+ 1);string+=String.fromCharCode(((c&31)<<6)|(c2&63));i+=2;}
-else{c2=utftext.charCodeAt(i+ 1);c3=utftext.charCodeAt(i+ 2);string+=String.fromCharCode(((c&15)<<12)|((c2&63)<<6)|(c3&63));i+=3;}}
-return string;}}
-var encode=document.getElementById('encode'),decode=document.getElementById('decode'),output=document.getElementById('output'),input=document.getElementById('input');var User_ID="";var protected_links="";var a_to_va=0;var a_to_vb=0;var a_to_vc="";function auto_safelink(){auto_safeconvert();}
-function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_links!=""&&!protected_links.match(a_to_vd)){protected_links+=", "+ a_to_vd;}else if(protected_links=="")
-{protected_links=a_to_vd;}
-var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
-{a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
-{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
-{a_to_vh=true;}
-j++;}
-if(a_to_vh==false)
-{var encryptedUrl=Base64.encode(a_to_vi);a_to_ve[i].href="https://www.animebatch.eu.org/2020/01/keuntungan-menggunakan-agen-branding.html?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
-var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
-{a_to_vj.innerHTML+=a_to_vb;}
-if(a_to_vk)
-{a_to_vk.innerHTML+=a_to_va;}}
-function a_to_fa()
-{var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Streaming Nanime.Tersenyum.di.Runway.01.480p.mp4</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <script src="/cdn-cgi/apps/head/Ndpx54TlGhYkCfM4hbPL-Af33BI.js"></script><link href="/plugins/videojs-logo.min.css" rel="stylesheet">
+  <link href="/plugins/videojs-resolution-switcher.css" rel="stylesheet">
+  <link href="/videojs/video-js.min.css" rel="stylesheet" type="text/css">
+  <link href="/plugins/videojs.ads.css" rel="stylesheet" type="text/css">
+  <link href="/plugins/videojs.ima.css" rel="stylesheet" type="text/css">
+  <link href="/skin/videojs-skin-flat.min.css" rel="stylesheet" type="text/css">
+
+  <script src="/videojs/ie8/videojs-ie8.min.js"></script>
+  <script src="/videojs/video.min.js"></script>
+  <style media="screen">
+  .vjs-watermark-text {
+    font-size: 20px !important;
+  }
+  </style>
+  <script>
+    videojs.options.flash.swf = "/videojs/video-js.swf";
+  </script>
+  
+</head>
+<body class="drive-viewer-fixed-frame">
+    
+      
+        <video id="topddl_player" class="video-js vjs-default-skin" controls preload="none" poster="https://moenime.id/wp-content/uploads/2020/02/Xian-Wang-de-Richang-Shenghuo-750x410.jpg">
+          
+            <source src="https://r4---sn-25glen7r.googlevideo.com/videoplayback?expire=1583713447&ei=JxxlXrD9CYzYhwa_xIHoDg&ip=2001:bc8:6005:1a:aa1e:84ff:fe96:9124&id=1fd9f598f33a3797&itag=18&source=blogger&mm=31&mn=sn-25glen7r&ms=au&mv=m&mvi=3&pl=44&susc=bl&mime=video/mp4&dur=1467.222&lmt=1578708898277202&mt=1583684549&sparams=expire,ei,ip,id,itag,source,susc,mime,dur,lmt&sig=ADKhkGMwRAIgZ5mUhLT997EDPY-Xvhecm4pLW_ffgLSrZl1a0tJPTCkCIFVzGZNlzelxjEN4W5ygQdUDxnslPDavE3X1FhEKYKVV&lsparams=mm,mn,ms,mv,mvi,pl&lsig=ABSNjpQwRAIgEIsQw4SziRylt7a0ouxWU1bw8fX_7-Tg4vZhNBhMcbwCIGqR2slTmeBFGM8L5jIIOw3zLeMzK27vyGiELtK6bocT" type='video/mp4' label='360' res='360' />
+          
+          <p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+        </video>
+        <script src="//imasdk.googleapis.com/js/sdkloader/ima3.js"></script>
+        <script src="/plugins/videojs-resolution-switcher.js"></script>
+        <script src="/plugins/videojs-logo.min.js"></script>
+        <script src="/plugins/videojs-hotkeys.js"></script>
+        <script src="/plugins/videojs.ads.min.js"></script>
+        <script src="/plugins/videojs.ima.min.js"></script>
+        <script>
+        var body = document.body,  html = document.documentElement;
+        var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+        var setup = {width: document.body.clientWidth, height};
+        
+        videojs('topddl_player', setup )
+        
+        videojs('topddl_player').videoLogo({
+          watermark: "Uservideo",
+        })
+        
+        
+        videojs('topddl_player').videoJsResolutionSwitcher();
+        videojs('topddl_player').hotkeys({
+          volumeStep: 0.1,
+          seekStep: 5,
+          enableModifiersForNumbers: false,
+          alwaysCaptureHotkeys: true
+        });
+        videojs('topddl_player').on('error', function(err) {
+          var current_time = videojs('topddl_player').currentTime();
+          
+          window.location = window.location.href.replace(/\&autoplay\=true\&time\=\d+\&retry\=\d+/g, '') + '&autoplay=true&time=' + current_time + '&retry=1';
+          // console.log(err);
+        })
+        
+          videojs('topddl_player').ima({
+            id: 'topddl_player',
+            adTagUrl: ' '
+          });
+        
+        </script>
+      
+    
+
+</body>
+</html>
